55c2e11cad74b57e15aee47eb6312f44f346bfdd
SgtTabouret <34285166+SgtTabouret@users.noreply.github.com>
2018-01-17 23:17:14 +0100

    Amelioration du calcul des degats

--- combat.java
+++ combat.java
@@ -1,7 +1,7 @@
 import extensions.CSVFile;
 class combat extends Program{
 //RAJOUTER FONCTION TEST POUR VERIFIER QUE LES REPONSES PROPOSES CONTIENNENT UNE BONNE
-String[] attaquesDispo = new String[] {"Charge (Histoire)","Morsure (Maths)","Griffe (Divers)","Tranche (Francais)"};
+String[] attaquesDispo = new String[] {"Charge (Histoire)","Morsure (Maths)","Griffe (Culture)","Tranche (Francais)"};
 String cheminCSV = "C:\\Users\\Flann\\Documents\\NetBeansProjects\\Pokeducation\\src\\Pokeducation\\";
 double facteurLv = 1;
 
@@ -18,26 +18,34 @@ double facteurLv = 1;
     }
     return tableau;
     }
-
-    String[][] conversionCsvQtoTabQ(String Attaque){
+    
+    String[][] conversionCsvQtoTabQ(String typeAttaque){
         String[][]tabQ=new String [50][50];
-              if (Attaque == "Charge (Histoire)"){
-                  CSVFile QHistoire = loadCSV(cheminCSV + "QHistoire.csv");
-                  tabQ=csvToTab(QHistoire);
-              }else if (Attaque == "Morsure (Maths)"){
-                 // CSVFile QMaths = loadCSV(cheminCSV + "QHMaths.csv");
-               //   tabQ=csvToTab(QMaths);
-              }else if (Attaque == "Griffe (Divers"){
-                 // CSVFile QDivers = loadCSV(cheminCSV + "QDivers.csv");
-                 // tabQ=csvToTab(QDivers);
-              }else if (Attaque == "Tranche (Francais)"){
-                  //CSVFile QFrancais = loadCSV(cheminCSV + "QFrancais.csv");
-                  //tabQ=csvToTab(QFrancais);
-              }
+        String nomCSV = "Q"+typeAttaque+".csv";
+        CSVFile questions = loadCSV(cheminCSV + nomCSV);
+        tabQ=csvToTab(questions);
         return tabQ;
     }
-
-
+    void testRecupererTypeAttaque(){
+        assertEquals("Francais", recupererTypeAttaque("Tranche (Francais)"));
+        assertEquals("Toto", recupererTypeAttaque("TuTu (Toto)"));
+    }
+    String recupererTypeAttaque(String Attaque) {
+        String typeAttaque="";
+        int i=0;
+        while (!")".equals(substring(Attaque,i,i+1))){
+            i=i+1;
+            if ("(".equals(substring(Attaque,i,i+1))){
+                i=i+1;
+                while (!")".equals(substring(Attaque,i,i+1))){
+                    typeAttaque=typeAttaque+substring(Attaque,i,i+1);
+                    i=i+1;
+                }
+            }
+        }
+        return typeAttaque;
+    }
+    
   void testChoixAttaque () {
     assertEquals("Charge",choixAttaque(1,attaquesDispo));
     assertEquals("Griffe",choixAttaque(3,attaquesDispo));
@@ -52,19 +60,22 @@ double facteurLv = 1;
     return attaqueChoisie;
   }
 
-  int calculDegats(String Attaque, int PATK, String pokeJoueur, String pokeEnnemi){
+  int calculDegats(String Attaque, Pokemon pokeEnnemi, Pokemon pokeJoueur){
     //calcul les degats infliges en fonction de la reussite de l'attaque
       int degats;
       boolean reussite;
       reussite = reussiteAttaque (Attaque);
-      degats = assignationDegats(reussite, PATK, pokeJoueur, pokeEnnemi);
+      degats = assignationDegats(reussite, pokeJoueur.PATK, pokeJoueur.Nom, pokeEnnemi.Nom, pokeJoueur.TypePoke);
       return degats;
   }
 
   boolean reussiteAttaque(String Attaque){
       //calcul la reussite de lattaque en fonction de la bonne ou mauvaise reponse du joueur
       boolean reussite = false;
-      String[][] tabQ =conversionCsvQtoTabQ(Attaque);
+      println("check1");
+      String typeAttaque=recupererTypeAttaque(Attaque);
+      println("check2");
+      String[][] tabQ =conversionCsvQtoTabQ(typeAttaque);
       int nQuestion = (int) (random()*length(tabQ,1));
       println(tabQ[nQuestion][0] + "\n");
       affichageRep(tabQ, nQuestion);
@@ -75,17 +86,17 @@ double facteurLv = 1;
       return reussite;
   }
   void testAssignationDegats(){
-      assertEquals(4, assignationDegats(true, 3, "poke1", "poke2"));
-      assertEquals(1,assignationDegats(false, 2, "poke1", "poke2"));
+      assertEquals(4, assignationDegats(true, 3, "poke1", "poke2", "histoire"));
+      assertEquals(1,assignationDegats(false, 2, "poke1", "poke2", "histoire"));
   }
-  int assignationDegats(boolean reussite,int PATK, String pokeJoueur, String pokeEnnemi){
+  int assignationDegats(boolean reussite,int PATK, String pokeJoueur, String pokeEnnemi, String typePokeJoueur){
      int degats;
      if (reussite == true){
          degats = PATK+1;
          println("Bien joué ! " + pokeJoueur + " a frappé " + pokeEnnemi + " de plein fouet ! ");
      } else {
          degats = (int) (PATK*0.5);
-         println("Mince ! " + pokeJoueur + " a raté son attaque !");
+         println("Mince ! " + pokeJoueur + " n'a pas lancé sa meilleure attaque !");
      }
      return degats;
   }
@@ -151,14 +162,15 @@ double facteurLv = 1;
         clearScreen();
         cursor(16,4);
         println(pokemonJoueur.Nom + " attaque " + attaqueLancee + "!");
-        delay(1000);
+        delay(800);
         clearScreen();
-        degats = calculDegats(attaqueLancee, pokemonJoueur.PATK, pokemonJoueur.Nom, pokemonApparu.Nom);
+        degats = calculDegats(attaqueLancee, pokemonApparu, pokemonJoueur);
 
         // perte de PVs
         pokemonApparu.PV = (int) (pokemonApparu.PV-degats);
         
         //riposte de l'ennemi
+        
         //fin combat quand l'ennemi ou le joueur n'a plus de PV
         //gain d'exp
         idxTour=idxTour+1;
